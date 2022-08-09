74b973f011a172bae3d5ae578af38801b7e48d16
imcoco <imcoco@gmail.com>
2022-08-09 16:59:19 -0400

    add create ui

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('npos/', views.npos_index, name='npos_index'),
-  path('npos/<int:npo_id>', views.npos_detail, name='npos_detail'),
+  path('npos/<int:npo_id>/', views.npos_detail, name='npos_detail'),
+  path('npos/create/', views.NPOCreate.as_view(), name='npos_create'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import NPO
+from django.views.generic.edit import CreateView
 
 # Create your views here.
 def home(request):
@@ -12,3 +13,7 @@ def npos_index(request):
 def npos_detail(request, npo_id):
   npo = NPO.objects.get(id=npo_id)
   return render(request, 'npos/detail.html', { 'npo': npo })
+
+class NPOCreate(CreateView):
+  model = NPO
+  fields = '__all__'

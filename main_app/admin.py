31c61d56c99c150a13c01df7551ef43863f3a5f0
imcoco <imcoco@gmail.com>
2022-09-01 15:54:32 -0400

    edit css for images

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import NPO, Event
+from .models import NPO, Event, Photo
 
 # Register your models here.
 admin.site.register(NPO)
 admin.site.register(Event)
+admin.site.register(Photo)
--- main_app/static/js/npo-detail.js
+++ main_app/static/js/npo-detail.js
@@ -1,3 +1,14 @@
+const fileInput = document.getElementById('file-input')
+const fileName = document.getElementById('file-name')
+
+fileInput.addEventListener('change', evt => {
+  const fileToUpload = evt.target.files[0].name
+  if(fileToUpload) {
+    fileName.innerText = fileToUpload
+  } else {
+    fileName.innerText = ""
+  }
+})
 const dateInput = document.getElementById('id_date')
 
 const picker = MCDatepicker.create({
@@ -9,16 +20,4 @@ const picker = MCDatepicker.create({
 
 dateInput.addEventListener("click", (evt) => {
   picker.open()
-})
-
-const fileInput = document.getElementById('file-input')
-const fileName = document.getElementById('file-name')
-
-fileInput.addEventListener('change', evt => {
-  const fileToUpload = evt.target.files[0].name
-  if(fileToUpload) {
-    fileName.innerText = fileToUpload
-  } else {
-    fileName.innerText = ""
-  }
 })
